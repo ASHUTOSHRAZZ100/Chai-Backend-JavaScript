700ad97e058e0fff40c29e20faa5a2edb1e21e56
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-02-13 16:14:49 +0530

    add custom Error and Response handler

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.db.js";
+import { app } from "./app.js";
 
 dotenv.config(
     {
@@ -7,4 +8,11 @@ dotenv.config(
     }
 )
 
-connectDB()
+connectDB().then(()=>{
+    const port = process.env.PORT || 5000;
+    app.listen(port,()=>{
+        console.log(`✔ Server is running at port: ${port} and at DB: ${process.env.MONGODB_URI}`)
+    })
+}).catch((err)=>{
+    console.log("❌ Server is not working ",err);
+});
