49a990e5e65a469bd2e7db81e5a194f05469e05c
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-02-15 15:17:30 +0530

    change CoverImageLocalPath logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,7 +33,11 @@ export const registerUser = asyncHandler(async (req, res, next) => {
 
   // check for image, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath = null;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+     coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar files is required");
