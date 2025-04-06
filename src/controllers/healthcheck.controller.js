e3340b3ede8b29a53830af970626181bb4302882
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-07 01:33:54 +0530

    Implement healthcheck endpoint to return a success message

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,6 +2,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const healthcheck = asyncHandler(async (req, res) => {});
+const healthcheck = asyncHandler(async (req, res) => {
+    res.status(200).json({message:"Looking Good"});
+});
 
 export { healthcheck };
