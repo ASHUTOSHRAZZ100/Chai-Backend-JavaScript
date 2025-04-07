b56e5ad637d53ac6b5a4b212ab1a74eb117d2159
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-07 19:05:29 +0530

    Disable secure flag for cookies in logoutUser to allow non-secure connections

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -200,7 +200,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   );
   const options = {
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite: "None", 
   };
   return res
